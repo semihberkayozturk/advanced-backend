bc43a32c62c17e794d6847e30ffae6a16a9d01ff
Semih Berkay Öztürk <ssemihberkay@gmail.com>
2022-10-22 14:59:05 +0300

    redis implementation

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,38 +1,38 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
-
 const Blog = mongoose.model('Blog');
+const cleanCache = require("../middlewares/cleanCache");
 
 module.exports = app => {
-  app.get('/api/blogs/:id', requireLogin, async (req, res) => {
-    const blog = await Blog.findOne({
-      _user: req.user.id,
-      _id: req.params.id
+    app.get('/api/blogs/:id', requireLogin, async(req, res) => {
+        const blog = await Blog.findOne({
+            _user: req.user.id,
+            _id: req.params.id
+        });
+
+        res.send(blog);
     });
 
-    res.send(blog);
-  });
+    app.get('/api/blogs', requireLogin, async(req, res) => {
+        const blogs = await Blog.find({ _user: req.user.id })
+            .cache({ key: req.user.id });
+        res.send(blogs)
+    })
 
-  app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
+    app.post('/api/blogs', requireLogin, cleanCache, async(req, res) => {
+        const { title, content } = req.body;
 
-    res.send(blogs);
-  });
+        const blog = new Blog({
+            title,
+            content,
+            _user: req.user.id
+        });
 
-  app.post('/api/blogs', requireLogin, async (req, res) => {
-    const { title, content } = req.body;
-
-    const blog = new Blog({
-      title,
-      content,
-      _user: req.user.id
+        try {
+            await blog.save();
+            res.send(blog);
+        } catch (err) {
+            res.send(400, err);
+        }
     });
-
-    try {
-      await blog.save();
-      res.send(blog);
-    } catch (err) {
-      res.send(400, err);
-    }
-  });
 };
